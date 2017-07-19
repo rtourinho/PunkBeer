1163bfb79a5145167b5eccacd3273dc54cf838bf
rtourinho <rtourinho@gmail.com>
2017-07-19 20:15:14 -0300

    Initial Commit

---
+++ Beer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beer
+//
+//  Created by Ricardo Tourinho on 19/07/17.
+//  Copyright © 2017 Ricardo Tourinho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
